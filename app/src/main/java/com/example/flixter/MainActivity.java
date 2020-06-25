c4abbb7ce76c59e0a77fe3926da51836c5f2b80d
eobbad <35933488+eobbad@users.noreply.github.com>
2020-06-25 09:05:46 -0700

    Began working on incorporating network components of Flixter app

---
+++ app/src/main/java/com/example/flixter/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.flixter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL =
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
